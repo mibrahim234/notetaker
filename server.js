44c63bb5cba235193dddbd98db48d2ce32e4e06d
mibrahim234 <mohamm1417@gmail.com>
2021-10-31 15:45:32 -0400

    added server foundation code

--- server.js
+++ server.js
@@ -1,16 +1,23 @@
 const express = require('express');
+// const apiRoutes = require('./routes/apiRoutes');
+// const htmlRoutes = require('./routes/htmlRoutes')
+
+ // create port--- initialize app 
 const PORT = process.env.PORT || 3001;
 const app = express();
-const path = require('path');
 
-// connect these to the html files
-const htmlFiles = require('./Develop/public');
-
+// Set up middleware 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
-// connect these two to the html files
-app.use('/', htmlFiles);
+app.use('/', htmlRoutes);
+app.use('/api', apiRoutes);
+
+// Starts server 
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
+  });
+
 
 //   The following HTML routes should be created:
 // * `GET /notes` should return the `notes.html` file.
@@ -23,7 +30,10 @@ app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));  
   });
 
+//   The following API routes should be created:
 
-app.listen(PORT, () => {
-  console.log(`API server now on port ${PORT}!`);
-});
+// * `GET /api/notes` should read the `db.json` file and return all saved notes as JSON.
+
+// * `POST /api/notes` should receive a new note to save on the request body, add it to the `db.json` file, and then return the new note to the client. You'll need to find a way to give each note a unique id when it's saved (look into `npm` packages that could do this for you).
+// uuid 
+
