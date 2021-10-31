035262661ba644f610eaf586ee7957cf437bf746
mibrahim234 <mohamm1417@gmail.com>
2021-10-31 15:29:36 -0400

    get routes

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
+const path = require('path');
 
 // connect these to the html files
 const htmlFiles = require('./Develop/public');
-
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -12,6 +12,16 @@ app.use(express.static('public'));
 // connect these two to the html files
 app.use('/', htmlFiles);
 
+//   The following HTML routes should be created:
+// * `GET /notes` should return the `notes.html` file.
+// * `GET *` should return the `index.html` file.
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, 'notes.html'));  
+  });
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));  
+  });
 
 
 app.listen(PORT, () => {
